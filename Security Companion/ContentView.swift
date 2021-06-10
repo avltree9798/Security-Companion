851637bff9f7283f19afe987eaeaca9f6576cb54
Anthony Viriya <anthony.viriya@go-pay.co.id>
2021-06-10 22:39:04 +0700

    Initial Commit

---
+++ Security Companion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Security Companion
+//
+//  Created by Anthony V on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Security Companion/Security_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Security_CompanionApp.swift
+//  Security Companion
+//
+//  Created by Anthony V on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Security_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
